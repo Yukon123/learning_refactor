3066c44b01d6c15cefdcee6ac7c42e316276e586
Yukon <zengyongkun@gmail.com>
2022-11-13 13:54:32 +0800

    更换为未重构的复杂例子

--- 6.1_Extract_Function.js
+++ 6.1_Extract_Function.js
@@ -1,19 +1,25 @@
 function printOwing(invoice) {
-    printBanner();
-    let outstanding = calculateOutstanding();
+    let outstanding = 0;
+
+    console.log("***********************");
+    console.log("**** Customer Owes ****");
+    console.log("***********************");
+
+    // calculate outstanding
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
+    }
+
+    // record due date
+    const today = Clock.today;
+    invoice.dueDate = new Date(
+        today.getFullYear(),
+        today.getMonth(),
+        today.getDate() + 30
+    );
 
     //print details
     console.log(`name: ${invoice.customer}`);
     console.log(`amount: ${outstanding}`);
-}
-
-function printOwing(invoice) {
-    printBanner();
-    let outstanding = calculateOutstanding();
-    printDetails(outstanding);
-
-    function printDetails(outstanding) {
-        console.log(`name: ${invoice.customer}`);
-        console.log(`amount: ${outstanding}`);
-    }
+    console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
