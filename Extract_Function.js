d974d0785bc99555044a1371218ad0a2ca2b3b33
Yukon <zengyongkun@gmail.com>
2022-11-13 13:44:38 +0800

    初始化提炼函数代码

---
+++ Extract_Function.js
@@ -0,0 +1,19 @@
+function printOwing(invoice) {
+    printBanner();
+    let outstanding = calculateOutstanding();
+
+    //print details
+    console.log(`name: ${invoice.customer}`);
+    console.log(`amount: ${outstanding}`);
+}
+
+function printOwing(invoice) {
+    printBanner();
+    let outstanding = calculateOutstanding();
+    printDetails(outstanding);
+
+    function printDetails(outstanding) {
+        console.log(`name: ${invoice.customer}`);
+        console.log(`amount: ${outstanding}`);
+    }
+}
